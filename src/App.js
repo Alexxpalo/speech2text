2bb8c018f4ad21011e6812a7706ce9d8b7bca7d5
Alexxpalo <palosaaritobias@gmail.com>
2022-11-01 13:07:18 +0200

    Test!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+var word = require('random-words');
+var currentWord = word();
+
+const App = () => {
+  const msg = new SpeechSynthesisUtterance();
+  msg.text = currentWord;
+  const [guess, setGuess] = useState('');
+  const [box, setBox] = useState('Click to start!');
+  const [score, setScore] = useState(0);
+
+  function getWord() {
+    setBox(currentWord.length);
+    console.log(currentWord);
+  }
+   
+  const handleGuess = event => {
+    setGuess(event.target.value);
+  }
+
+  const handleClick = event => {
+    event.preventDefault();
+
+    if(guess === currentWord){
+      currentWord = word();
+      setScore(score + 1);
+      getWord();
+      msg.text = currentWord;
+      window.speechSynthesis.speak(msg);
+    }  else {
+      setScore(score - 1);
+      window.speechSynthesis.speak(msg);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="content">
+
+        <div id="word" onClick={() => {getWord(); window.speechSynthesis.speak(msg);}}>
+          <p> {box} </p>
+        </div>
+
+        <div id="input">
+          <input type="text" id='guess' onChange={handleGuess} value={guess}></input>
+        </div>
+
+        <div id="send" onClick={handleClick}><p>Check!</p></div>
+
+        <div id="score"><p>{score}</p></div>
+      </div>
     </div>
   );
 }
